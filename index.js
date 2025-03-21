76c3b47b2edb5f1a91e98d4326618ec7eb3bf1c2
Nelson <nelson.atiang@student.moringaschool.com>
2025-03-21 23:44:37 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+// for(let age = 30;age<40;age++){
+// console.log(`I'm ${age} years old,Happy birthday to me `);
+
+// // }
+
+let arrays=[]
+function writeCards(name,event){
+    
+    for(let i = 0;i< name.length;i++){
+arrays.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+
+    }
+    return arrays
+}
+//let names=["Guadalupe", "Ollie", "Aki"]
+//writeCards("names","surprise")
+//writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(number){
+while(number >=0){
+    console.log (number);
+number--
+}
+
+}
+countDown(10)
